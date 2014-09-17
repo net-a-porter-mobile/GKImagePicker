f97078ec6877ac6099a6ce5aa75b0d295a259750
Erica Giordo <erica.giordo@net-a-porter.com>
2014-09-17 17:09:59 +0100

    Removed red color used for testing

--- GKClasses/GKImageCropView.m
+++ GKClasses/GKImageCropView.m
@@ -243,7 +243,6 @@ static CGRect GKScaleRect(CGRect rect, CGFloat scale)
 
     self.cropOverlayView.frame = self.bounds;
     self.scrollView.frame = CGRectMake(xOffset, yOffset + 46, size.width, size.height);
-    self.scrollView.backgroundColor = [UIColor redColor];
     self.scrollView.contentSize = CGSizeMake(size.width, size.height);
     self.imageView.frame = CGRectMake(0, floor((size.height - faktoredHeight) * 0.5), faktoredWidth, faktoredHeight);
 }
