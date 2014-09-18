622c1eaa3736b9624b99d0bb73f091ce8391365b
Erica Giordo <erica.giordo@net-a-porter.com>
2014-09-18 15:58:37 +0100

    Fixed padding in GKImageCropView. Removed bottom tab bar. Defined buttons in navigation bar

--- GKClasses/GKImageCropView.m
+++ GKClasses/GKImageCropView.m
@@ -242,7 +242,8 @@ static CGRect GKScaleRect(CGRect rect, CGFloat scale)
     }
 
     self.cropOverlayView.frame = self.bounds;
-    self.scrollView.frame = CGRectMake(xOffset, yOffset + 46, size.width, size.height);
+    
+    self.scrollView.frame = CGRectMake(xOffset, yOffset - 18.0f, size.width, size.height);
     self.scrollView.contentSize = CGSizeMake(size.width, size.height);
     self.imageView.frame = CGRectMake(0, floor((size.height - faktoredHeight) * 0.5), faktoredWidth, faktoredHeight);
 }
